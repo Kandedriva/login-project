d71c99f5581a00df1f95e16cc39cb1c0e19acca1
drissakande <114373066+Kandedriva@users.noreply.github.com>
2024-03-25 14:38:49 -0400

    Create the Node.js server and installExpress and ejs. Create and write code in login.ejs

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
+
+
+const port = 3000
+const app = express()
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req, res)=>{
+    res.render("login.ejs")
+})
+
+app.listen(port, ()=>{
+    console.log(`The server is running on ${port}`)
+})
